134958c922500f781ceb2dca3d60062bb2364a31
b3m3 <b3m3@yandex.ru>
2022-06-24 19:02:38 +0300

    showRangeValue func

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  const numbers = document.querySelector('#numbers');
+  const symbols = document.querySelector('#symbols');
+  const range = document.querySelector('#range');
+  const button = document.querySelector('.settings__btn');
+
+  (function showRangeValue()  {
+    const counter = document.querySelector('.range-label span');
+    range.oninput = () => {
+      counter.textContent = range.value;
+    }
+  })();
+
   
 });
